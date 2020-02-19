bcdd1fa0f5bc07bd94807a8744b9475ec2d7ed0a
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-02-19 04:29:19 -0500

    New Unity Project

---
+++ DeckcendantFinal/Assets/Scripts/Hand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public int[] hand;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ DeckcendantFinal/Assets/Scripts/Hover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    protected int idle;
+
+    public void OnHover()
+    {
+        idle = GetComponent<Canvas>().sortingOrder;
+        GetComponent<Canvas>().sortingOrder = 6;
+        GetComponent<Animator>().Play("CardHover");
+    }
+
+    public void OnHoverExit()
+    {
+        GetComponent<Canvas>().sortingOrder = idle;
+        GetComponent<Animator>().Play("CardHoverExit");
+    }
+}
