91fd3760d44563ec47dfa8e1fa50a3b9b40468be
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-03-04 15:28:36 -0500

    f u GitHub

---
+++ DeckcendantFinal/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void setScore(int value)
+    {
+        score = value;
+    }
+    public int getScore()
+    {
+        return score;
+    }
+}
+
