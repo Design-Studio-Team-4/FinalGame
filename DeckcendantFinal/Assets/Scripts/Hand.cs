4ebd8eeefa3d2bde7a3be0f2f36327e123a0d031
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-03-04 15:26:20 -0500

    Card Mechanics and Enemies

---
+++ DeckcendantFinal/Assets/Scripts/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public GameObject cardManager;
+    private CardManager cardManagerScript;
+
+    void Start()
+    {
+        cardManagerScript = cardManager.GetComponent<CardManager>();
+    }
+
+    public void Draw()
+    {
+        cardManagerScript.Draw();
+    }
+}
