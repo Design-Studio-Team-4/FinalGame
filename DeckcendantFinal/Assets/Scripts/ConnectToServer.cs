6afe8280245d66ebe6035f7ff6b346b99a1c0c53
Darksparky <Darksparky@users.noreply.github.com>
2020-03-04 13:22:11 -0500

    addednew scene and some scripts for leaderboard

---
+++ DeckcendantFinal/Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+public class ConnectToServer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(connect());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator connect(){
+        using(UnityWebRequest www = UnityWebRequest.Get("http://localhost/Deckcendant/connect.php")){
+            yield return www.Send();
+            if (www.isNetworkError|| www.isHttpError){
+                Debug.Log(www.error);
+
+            }else{
+                Debug.Log(www.downloadHandler.text);
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+   
+}
