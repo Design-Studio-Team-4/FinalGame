ee43694f0e90ece699dd8c8c5adf2988c8714d08
Darksparky <Darksparky@users.noreply.github.com>
2020-03-24 19:31:09 -0400

    did submit button check

--- DeckcendantFinal/Assets/Scripts/ConnectToServer.cs
+++ DeckcendantFinal/Assets/Scripts/ConnectToServer.cs
@@ -3,34 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 
 public class ConnectToServer : MonoBehaviour
 {
+    public TMPro.TMP_InputField Inputfield;
+    public GameObject placeholdertext;
+    public GameObject LeaderboardCanvas;
+    public GameObject SubmissionCanvas;
     int score = 1;
-    string name = "Chris";
+    string name = " ";
+    bool submitted = false;
+    bool ranConnect = false;
 
     public TMPro.TextMeshProUGUI Leaderboard;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(connect());
-        StartCoroutine(RunLeaderboard(name, score));
+        SubmissionCanvas.SetActive(true);
+        LeaderboardCanvas.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (submitted == true)
+        {
+            if (ranConnect == false)
+            {
+               // StartCoroutine(connect());
+                StartCoroutine(RunLeaderboard(name, score));
+                SubmissionCanvas.SetActive(false);
+                LeaderboardCanvas.SetActive(true);
+                ranConnect = true;
+            }
+  
+        }
     }
-    IEnumerator connect(){
-        using(UnityWebRequest www = UnityWebRequest.Get("http://localhost/Deckcendant/connect.php")){
+    IEnumerator connect() {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/Deckcendant/connect.php")) {
             yield return www.Send();
-            if (www.isNetworkError|| www.isHttpError){
+            if (www.isNetworkError || www.isHttpError) {
                 Debug.Log(www.error);
 
-            }else{
+            } else {
                 Debug.Log(www.downloadHandler.text);
                 byte[] results = www.downloadHandler.data;
             }
@@ -55,22 +74,37 @@ public class ConnectToServer : MonoBehaviour
                 Leaderboard.text = www.downloadHandler.text;
                 byte[] results = www.downloadHandler.data;
             }
-            
-           
+
+
         }
     }
     public void NextPage()
     {
         Leaderboard.pageToDisplay++;
     }
-    
+
     public void PreviousPage()
     {
         if (Leaderboard.pageToDisplay > 0)
         {
             Leaderboard.pageToDisplay--;
         }
-            
+
     }
-}
+    public void SubmitName()
+    {
+        if (string.IsNullOrEmpty(Inputfield.text) != true)
+        {
+            name = Inputfield.text;
+            submitted = true;
+
+        }
+        else
+        {
+            placeholdertext.GetComponent<TMPro.TextMeshProUGUI>().text = "Invalid";
+        }
+   
+    }
+} 
+
 
