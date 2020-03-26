5e8910a922307dc7eadcca036cbedbfec38366d7
benfowlercool <60189784+benfowlercool@users.noreply.github.com>
2020-03-25 21:20:40 -0400

    Shop

--- DeckcendantFinal/Assets/Scripts/shop.cs
+++ DeckcendantFinal/Assets/Scripts/shop.cs
@@ -2,94 +2,84 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
-// so we can manipulte item in unity inspector
-[System.Serializable]
-public class Item
-{
-    public string itemName;
-    public Sprite icon;
-    public float price = 1f;
-}
-
-public class ShopScrollList : MonoBehaviour
+public class shop : MonoBehaviour
 {
 
-    public List<Item> itemList;
-    public Transform contentPanel;
-    public ShopScrollList otherShop;
-    public Text myGoldDisplay;
-    public SimpleObjectPool buttonObjectPool;
+    public Transform Purchase1;
+    public Transform Purchase2;
+    public Transform Purchase3;
+    public Transform PurchaseField;
+    public float CARDTOTAL = 10;
 
-    public float gold = 10f;
+    // initiates shop elements
+    public void Awake()
+    {
+        PurchaseField = transform.Find("PurchaseField");
+        Purchase1 = PurchaseField.Find("Purchase1");
+        Purchase1.gameObject.SetActive(false);
+        Purchase2 = PurchaseField.Find("Purchase2");
+        Purchase2.gameObject.SetActive(false);
+        Purchase3 = PurchaseField.Find("Purchase3");
+        Purchase3.gameObject.SetActive(false);
+    }
 
-
-    // Use this for initialization
     void Start()
     {
-        RefreshDisplay();
+        Awake();
+
+        ChooseButton(Purchase1);
+        ChooseButton(Purchase2);
+        ChooseButton(Purchase3);
+
     }
 
-    void RefreshDisplay()
+    public void CreateButton(Sprite cardFronts, string name, int cost, UnityEngine.Transform purchase)
     {
-        myGoldDisplay.text = "Gold: " + gold.ToString();
-        RemoveButtons();
-        AddButtons();
+        Transform PurchaseFieldTransform = Instantiate(purchase, PurchaseField);
+        PurchaseFieldTransform.gameObject.SetActive(true);
+
+        // set name and cost
+        PurchaseFieldTransform.Find("name").GetComponent<TextMeshProUGUI>().SetText(name);
+        PurchaseFieldTransform.Find("cost").GetComponent<TextMeshProUGUI>().SetText(cost.ToString());
+
+        // set front
+        PurchaseFieldTransform.Find("Image").GetComponent<Image>().sprite = cardFronts;
+
+        PurchaseFieldTransform.GetComponent<Button>();
     }
 
-    private void RemoveButtons()
+    public void ChooseButton(UnityEngine.Transform purchase)
     {
-        while (contentPanel.childCount > 0)
-        {
-            GameObject toRemove = transform.GetChild(0).gameObject;
-            buttonObjectPool.ReturnObject(toRemove);
-        }
+        var num = Random.Range(1, CARDTOTAL + 1);
+        Debug.Log(num);
+
+        if (num >= 10) CreateButton(CardManager.cManagerInstance.cardFronts[9], "Turn The Tides", 3, purchase);
+        else if (num >= 9) CreateButton(CardManager.cManagerInstance.cardFronts[8], "Time Steal", 3, purchase);
+        else if (num >= 8) CreateButton(CardManager.cManagerInstance.cardFronts[7], "Stasis", 3, purchase);
+        else if (num >= 7) CreateButton(CardManager.cManagerInstance.cardFronts[6], "Soulfire Sacrifice", 3, purchase);
+        else if (num >= 6) CreateButton(CardManager.cManagerInstance.cardFronts[5], "Shield Ward", 2, purchase);
+        else if (num >= 5) CreateButton(CardManager.cManagerInstance.cardFronts[4], "Hail Of Daggers", 3, purchase);
+        else if (num >= 4) CreateButton(CardManager.cManagerInstance.cardFronts[3], "First Strike", 3, purchase);
+        else if (num >= 3) CreateButton(CardManager.cManagerInstance.cardFronts[2], "Heal", 3, purchase);
+        else if (num >= 2) CreateButton(CardManager.cManagerInstance.cardFronts[1], "Block", 2, purchase);
+        else if (num >= 1) CreateButton(CardManager.cManagerInstance.cardFronts[0], "Strike", 1, purchase);
+        
+      
+
     }
 
-    private void AddButtons()
-    {
-        for (int i = 0; i < itemList.Count; i++)
-        {
-            Item item = itemList[i];
-            GameObject newButton = buttonObjectPool.GetObject();
-            newButton.transform.SetParent(contentPanel);
-
-            SampleButton sampleButton = newButton.GetComponent<SampleButton>();
-            sampleButton.Setup(item, this);
-        }
-    }
-
-    public void TryTransferItemToOtherShop(Item item)
-    {
-        if (otherShop.gold >= item.price)
-        {
-            gold += item.price;
-            otherShop.gold -= item.price;
-
-            AddItem(item, otherShop);
-            RemoveItem(item, this);
-
-            RefreshDisplay();
-            otherShop.RefreshDisplay();
-            Debug.Log("enough gold");
-
-        }
-        Debug.Log("attempted");
-    }
-
-    void AddItem(Item itemToAdd, ShopScrollList shopList)
-    {
-        shopList.itemList.Add(itemToAdd);
-    }
-
-    private void RemoveItem(Item itemToRemove, ShopScrollList shopList)
-    {
-        for (int i = shopList.itemList.Count - 1; i >= 0; i--)
-        {
-            if (shopList.itemList[i] == itemToRemove)
-            {
-                shopList.itemList.RemoveAt(i);
-            }
-        }
-    }
+    //public void OnClick
+    //{
+    //    //if (score => cost)
+    //    //{
+    //        // give player card
+    //        //CardManager.cManagerInstance.deck.Add(new Card("Heal", 1, 5, false, false, cardFronts[2], cardBacks[2], "Heals 5 health"));
+    //        // subtract score
+    //        //score - cost = score;
+    //        // remove button
+    //    //}
+    //}
+    
 }
