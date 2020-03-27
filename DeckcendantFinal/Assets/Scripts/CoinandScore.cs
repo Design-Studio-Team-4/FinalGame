5de3b8059be6f0ad0813182db54baf79adb23436
Dylarno <60194054+Dylarno@users.noreply.github.com>
2020-03-27 04:48:58 -0400

    almost there

---
+++ DeckcendantFinal/Assets/Scripts/CoinandScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinandScore : MonoBehaviour
+{
+    public static CoinandScore coinsAndScoreInstance;
+
+    public int coins;
+    public int score;
+
+    void Awake()
+    {
+        if (coinsAndScoreInstance == null) { coinsAndScoreInstance = this; }
+
+        coins = 0;
+        score = 0;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
